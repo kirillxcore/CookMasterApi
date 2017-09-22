3bc7207e748a5e107e0b4f1f21531f84d9d5e2fe
kirill.merkulets <kirill.merkulets@fastdev.se>
2017-09-22 09:29:55 +0400

    test

--- CookMasterApi/Controllers/ValuesController.cs
+++ CookMasterApi/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using CookFormMaster;
 
 namespace CookMasterApi.Controllers
 {
@@ -18,6 +19,8 @@ namespace CookMasterApi.Controllers
         // GET api/values/5
         public string Get(int id)
         {
+            CookFormManager cookFormManager = new CookFormManager();
+            cookFormManager.Test();
             return "Hello ;)";
         }
 
---
+++ CookTests/Tester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CookFormMaster;
+using NUnit.Framework;
+
+namespace CookTests
+{
+    public class Tester
+    {
+        [Test]
+        public void Test()
+        {
+            CookFormManager cookFormManager = new CookFormManager();
+            cookFormManager.Test();
+        }
+    }
+}
