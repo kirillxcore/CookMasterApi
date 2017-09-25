b4f42d96f07855c576e4c651851e273887f17418
DmitryAliskerov <dmitry.aliskerov@apicasystem.com>
2017-09-25 15:33:10 +0400

    Test

--- CookMasterApi/Controllers/HomeController.cs
+++ CookMasterApi/Controllers/HomeController.cs
@@ -6,11 +6,60 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using CookFormMaster;
+using Google.Apis.Auth.OAuth2.Flows;
 using Google.Apis.Auth.OAuth2.Mvc;
+using Google.Apis.Auth.OAuth2.Web;
 
 namespace CookMasterApi.Controllers
 {
-    public class HomeController : AsyncController
+	public class MyAuthorizationCodeMvcApp : AuthorizationCodeWebApp
+	{
+		private readonly Controller controller;
+		private readonly FlowMetadata flowData;
+
+		/// <summary>Gets the controller which is the owner of this authorization code MVC app instance.</summary>
+		public Controller Controller
+		{
+			get
+			{
+				return this.controller;
+			}
+		}
+
+		/// <summary>Gets the <see cref="T:Google.Apis.Auth.OAuth2.Mvc.FlowMetadata" /> object.</summary>
+		public FlowMetadata FlowData
+		{
+			get
+			{
+				return this.flowData;
+			}
+		}
+
+		/// <summary>Constructs a new authorization code MVC app using the given controller and flow data.</summary>
+		public MyAuthorizationCodeMvcApp(Controller controller, FlowMetadata flowData)
+			: base(flowData.Flow, "http://cookmaster.apphb.com/AuthCallback/IndexAsync", controller.Request.Url.ToString())
+		{
+			this.controller = controller;
+			this.flowData = flowData;
+		}
+
+		/// <summary>
+		/// Asynchronously authorizes the installed application to access user's protected data. It gets the user
+		/// identifier by calling to <see cref="M:Google.Apis.Auth.OAuth2.Mvc.FlowMetadata.GetUserId(System.Web.Mvc.Controller)" /> and then calls to
+		/// <see cref="M:Google.Apis.Auth.OAuth2.Web.AuthorizationCodeWebApp.AuthorizeAsync(System.String,System.Threading.CancellationToken)" />.
+		/// </summary>
+		/// <param name="taskCancellationToken">Cancellation token to cancel an operation</param>
+		/// <returns>
+		/// Auth result object which contains the user's credential or redirect URI for the authorization server
+		/// </returns>
+		public Task<AuthorizationCodeWebApp.AuthResult> AuthorizeAsync(CancellationToken taskCancellationToken)
+		{
+			return this.AuthorizeAsync(this.FlowData.GetUserId(this.Controller), taskCancellationToken);
+		}
+	}
+
+
+	public class HomeController : AsyncController
     {
      /*   public ActionResult Index()
         {
@@ -21,10 +70,8 @@ namespace CookMasterApi.Controllers
 */
         public async Task<ActionResult> Index(CancellationToken cancellationToken)
         {
-            var result = await new AuthorizationCodeMvcApp(this, new AppFlowMetadata()).
+            var result = await new MyAuthorizationCodeMvcApp(this, new AppFlowMetadata()).
                 AuthorizeAsync(cancellationToken);
-
-	        return View("Index");
 
 			if (result.Credential != null)
             {
@@ -34,7 +81,7 @@ namespace CookMasterApi.Controllers
 
 				ViewBag.Message =  cookFormManager.Test();
 
-
+	            return View("Index");
 			}
             else
             {
